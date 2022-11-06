c7a1e7701ce5dd8b4c8d9daea689647a327c3c68
Bolek <103572991+BoloMasta@users.noreply.github.com>
2022-11-06 23:21:11 +0100

    initial commit
    build html
    add styles
    base of quotes

---
+++ app.js
@@ -0,0 +1,10 @@
+const quotes = [
+  "Some people feel the rain. Others just get wet.",
+  "Love the life you live. Live the life you love.",
+  "Emancipate yourselves from mental slavery. None but ourselves can free our minds.",
+  "Live for yourself and you will live in vain; Live for others, and you will live again.",
+  "I no have education. I have inspiration. If I was educated I would be a damn fool.",
+  "Better to die fighting for freedom than be a prisoner all the days of your life.",
+  "Open your eyes, look within. Are you satisfied with the life you’re living?"
+];
+
