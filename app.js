3d706f572f4f49bec7b48fd79738fdc87a8f19d3
Bolek <103572991+BoloMasta@users.noreply.github.com>
2022-11-07 00:10:07 +0100

    build app
    set variables
    feat randomQuotes

--- app.js
+++ app.js
@@ -5,6 +5,33 @@ const quotes = [
   "Live for yourself and you will live in vain; Live for others, and you will live again.",
   "I no have education. I have inspiration. If I was educated I would be a damn fool.",
   "Better to die fighting for freedom than be a prisoner all the days of your life.",
-  "Open your eyes, look within. Are you satisfied with the life you’re living?"
+  "Open your eyes, look within. Are you satisfied with the life you’re living?",
 ];
 
+const quote = document.querySelector(".quote");
+const input = document.querySelector(".input");
+const results = document.querySelector(".results");
+let quoteWordsArray = [];
+
+const randomQuote = () => {
+  const randomIndex = Math.floor(Math.random() * quotes.length);
+  quote.textContent = '"' + quotes[randomIndex] + '"';
+  quoteWordsArray = quotes[randomIndex].split(" ").map((word) => word + " ");
+  quoteWordsArray[quoteWordsArray.length - 1] = quoteWordsArray[quoteWordsArray.length - 1].trim();
+};
+
+const checkInput = () => {
+  const inputValue = input.value;
+  let wordArray = inputValue.split("");
+
+  if (inputValue === wordArray[0]) {
+    wordArray.shift();
+    input.style.backgroundColor = "white";
+  } else {
+    input.style.backgroundColor = "red";
+  }
+};
+
+randomQuote();
+
+input.addEventListener("input", checkInput);
